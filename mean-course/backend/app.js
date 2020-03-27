124a8ac8c6e26524ed233e2e67ff1df95e56c04f
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-03-27 15:08:50 +0100

    Posting messages to DB

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -36,22 +36,14 @@ app.post('/api/posts',(req, res, next) => {
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-    id: 1,
-    title: 'Title 1',
-    content: 'Message 1 from server'
-    },
-    {
-      id: 2,
-      title: 'Title 2',
-      content: 'Message 2 from server'
-      }
-];
-  res.status(200).json({
-    message: 'Post fetched succesfully',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Post fetched succesfully',
+        posts: documents
+      });
+    });
+
 });
 
 module.exports = app;
