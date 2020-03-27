bb7c6bd23125d96660edcb0b3638d23dff4e3ec7
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-03-27 11:52:16 +0100

    Setting path to fetch posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+    id: 1,
+    title: 'Title 1',
+    content: 'Message 1 from server'
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      content: 'Message 2 from server'
+      }
+];
+  res.status(200).json({
+    message: 'Post fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
