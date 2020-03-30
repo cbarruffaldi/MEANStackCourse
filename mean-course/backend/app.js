de8a2ca35fc8c3a16f7bc43f6cc2b01e57154c65
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-03-30 12:51:41 +0200

    Update messages

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
 });
 
@@ -55,5 +55,27 @@ app.delete('/api/posts/:id', (req, res, next) => {
   });
 });
 
+
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Update successful'});
+  });
+});
+
+app.get('/api/posts/:id', (req, res, post) => {
+  Post.findById(req.params.id).then(post => {
+    if( post) {
+      res.status(200).json(post);
+    } else {
+      req.status(400).json({message: 'Post not found'});
+    }
+  });
+})
 
 module.exports = app;
