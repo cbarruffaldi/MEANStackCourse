91176e5209f59bce427d58e657c9855e4ff145f8
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-04-09 19:04:49 +0200

    Error handling Signup and signin

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -37,7 +37,9 @@ export class AuthService {
     const authData: AuthData = {email: email, password: password};
     this.http.post('http://localhost:3000/api/user/signup', authData)
       .subscribe(response => {
-        console.log(response);
+        this.router.navigate['/'];
+      }, error => {
+        this.authStatusListener.next(false);
       })
   }
 
@@ -58,6 +60,8 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+    }, error => {
+      this.authStatusListener.next(false);
     })
   }
 
--- mean-course/src/app/auth/signup/signup.component.ts
+++ mean-course/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub: Subscription;
 
   constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authStatusSub =  this.authService.getAuthStatusListener().subscribe(
+      authStates => {
+        this.isLoading = false;
+      }
+    );
+  }
 
   onSignup(form: NgForm) {
     if(form.invalid) {
@@ -17,5 +28,9 @@ export class SignupComponent {
     }
     this.isLoading = true;
     this.authService.createuser(form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
