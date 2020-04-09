cd217772278117edfc581f411707ed352f54e488
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-04-09 19:44:16 +0200

    Returning error messages on the server

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -43,6 +43,11 @@ router.post('', checkAuth, multer({storage: storage}).single('image'),(req, res,
         id: createdPost._id
       }
     });
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Creating a post failed',
+    })
   });
 });
 
@@ -66,6 +71,11 @@ router.get('',(req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Fetching posts failed'
+      });
     });
 
 });
@@ -77,7 +87,12 @@ router.delete('/:id', checkAuth, (req, res, next) => {
     } else {
       res.status(401).json({message: 'Not Authorized'});
     }
-  });
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Fetching posts failed',
+    })
+  });;
 });
 
 
@@ -100,7 +115,12 @@ router.put('/:id', checkAuth, multer({storage: storage}).single('image'), (req, 
     } else {
       res.status(401).json({message: 'Not Authorized'});
     }
-  });
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Could not update post',
+    })
+  });;
 });
 
 router.get('/:id', (req, res, post) => {
@@ -110,7 +130,13 @@ router.get('/:id', (req, res, post) => {
     } else {
       req.status(400).json({message: 'Post not found'});
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Fetching posts failed'
+    });
   });
+
 })
 
 module.exports = router;
