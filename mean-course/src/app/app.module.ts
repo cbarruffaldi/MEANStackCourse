6531156bdae3ee2517157a3cbf1935c7eb75e8a1
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-04-10 16:40:18 +0200

    Lazy Loading for Auth

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular.material.module';
 import { PostsModule } from './posts/post.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -25,7 +24,6 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AngularMaterialModule,
     PostsModule,
-    AuthModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
