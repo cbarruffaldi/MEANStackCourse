48a9a23c194b29f69916684aa26bef4851a8cd51
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-04-10 15:22:46 +0200

    Material Module component

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,17 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
-
-
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,6 +13,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular.material.module';
 
 @NgModule({
   declarations: [
@@ -38,18 +29,10 @@ import { ErrorComponent } from './error/error.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    MatDialogModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
     FormsModule,
-
+    AngularMaterialModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
